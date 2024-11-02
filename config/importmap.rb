f5cae1cdc61057db7023c60d59bfd9ce62259ef9
marius-hernandez <21-07121@g.batstate-u.edu.ph>
2024-11-02 08:33:10 +0800

    chore - import shadcn and flowbite

---
+++ config/importmap.rb
@@ -0,0 +1,6 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "flowbite", to: "https://ga.jspm.io/npm:flowbite@2.5.2/lib/esm/index.js"
+pin "application"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+pin "flowbite-datepicker", to: "https://ga.jspm.io/npm:flowbite-datepicker@1.3.0/dist/main.esm.js"
---
+++ config/tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const shadcnConfig = require("./shadcn.tailwind.js");
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ],
+  ...shadcnConfig,
+}
